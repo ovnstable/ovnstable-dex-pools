ad4430fa9c416c71e98e4ba93e5849dca3d25ecb
Roman <romangolovlyov@gmail.com>
2023-07-18 11:25:33 +0500

    dracula updated

--- src/adapters/exchangers/dracula.service.ts
+++ src/adapters/exchangers/dracula.service.ts
@@ -1,66 +1,81 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PoolData } from './dto/pool.data.dto';
-import axios from 'axios';
 import { ExchangerRequestError } from '../../exceptions/exchanger.request.error';
 import { ExchangerType } from '../../exchanger/models/inner/exchanger.type';
 import { AdaptersService } from '../adapters.service';
 import { ChainType } from '../../exchanger/models/inner/chain.type';
+import fetch from 'node-fetch';
+import axios from 'axios';
 
 @Injectable()
 export class DraculaService {
     private readonly logger = new Logger(DraculaService.name);
-    // add link to dracula and form a link to a pool or api
     BASE_API_URL = 'https://api-dex.draculafi.xyz';
     METHOD_GET_PAIRS = 'pairs';
 
     async getPoolsData(): Promise<PoolData[]> {
-        // Form a link here
-        const url = `${this.BASE_API_URL}/${this.METHOD_GET_PAIRS}`;
-        console.log("Load data by URL:", url);
+        const apiUrl =
+            "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cusd-coin%2Cusd&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&precision=full";
 
-        const response = axios
-            .get(url, {
+        try {
+            const coinGeckoResponse = await fetch(apiUrl);
+            if (!coinGeckoResponse.ok) {
+                throw new Error("Error: " + coinGeckoResponse.status);
+            }
+
+            const coinGeckoData = await coinGeckoResponse.json();
+            const ethereumPrice = coinGeckoData.ethereum.usd;
+            const usdCoinPrice = coinGeckoData["usd-coin"].usd;
+            const usdPrice = coinGeckoData.usd.usd;
+
+            const url = `${this.BASE_API_URL}/${this.METHOD_GET_PAIRS}`;
+            console.log("Load data by URL:", url);
+
+            const response = await axios.get(url, {
                 timeout: 80_000, // 80 sec
-            })
-            .then((data): PoolData[] => {
-                const pools: PoolData[] = [];
-//        console.log('Response data: ', data.data);
-                const pairs = data.data.pairs;
-                let itemCount = 0;
-                pairs.forEach((item) => {
-                    if (
-                        item &&
-                        item.symbol &&
-                        AdaptersService.OVN_POOLS_NAMES.some((str) =>
-                            item.symbol.toLowerCase().includes(str),
-                        )
-                    ) {
-                        const poolData: PoolData = new PoolData();
-
-                        poolData.address = item.address;
-                        poolData.name = item.symbol;
-                        poolData.decimals = item.decimals;
-                        poolData.tvl = (item.reserve0 * 1 + item.reserve1 * 1).toString();
-                        poolData.apr = null;
-                        poolData.chain = ChainType.ZKSYNC;
-                        pools.push(poolData);
-                        this.logger.log(`========= ${ExchangerType.DRACULA} =========`);
-                        itemCount++;
-                        this.logger.log('Found ovn pool #: ', itemCount);
-                        this.logger.log('Found ovn pool: ', poolData);
-                        this.logger.log('==================');
-                    }
-                });
-
-                return pools;
-            })
-            .catch((e) => {
-                const errorMessage = `Error when load ${ExchangerType.DRACULA} pairs.`;
-                this.logger.error(errorMessage, e);
-                throw new ExchangerRequestError(errorMessage);
             });
 
-        return await response;
+            const pools: PoolData[] = [];
+            const pairs = response.data.pairs;
+            let itemCount = 0;
+
+            pairs.forEach((item) => {
+                if (
+                    item &&
+                    item.symbol &&
+                    AdaptersService.OVN_POOLS_NAMES.some((str) =>
+                        item.symbol.toLowerCase().includes(str)
+                    )
+                ) {
+                    const poolData: PoolData = new PoolData();
+
+                    poolData.address = item.address;
+                    poolData.name = item.symbol;
+                    poolData.decimals = item.decimals;
+                    if (item.symbol === "vAMM-WETH/USD+") {
+                        poolData.tvl = (item.reserve0 * ethereumPrice + item.reserve1 * usdPrice).toString();
+                    } else {
+                        poolData.tvl = (item.reserve0 * usdCoinPrice + item.reserve1 * usdPrice).toString();
+                    }
+                    poolData.apr = null;
+                    poolData.chain = ChainType.ZKSYNC;
+
+                    pools.push(poolData);
+
+                    this.logger.log(`========= ${ExchangerType.DRACULA} =========`);
+                    itemCount++;
+                    this.logger.log('Found ovn pool #: ', itemCount);
+                    this.logger.log('Found ovn pool: ', poolData);
+                    this.logger.log('==================');
+                }
+            });
+
+            return pools;
+        } catch (error) {
+            console.error(error);
+            throw new ExchangerRequestError(
+                `Error when loading ${ExchangerType.DRACULA} pairs.`
+            );
+        }
     }
 }
-
