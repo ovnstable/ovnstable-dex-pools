4cffc3162c3a8e07d4bcadaf59b49672a33c844a
dmitry <dmitry.timoschuk@gmail.com>
2023-10-04 13:20:59 +0400

    beefy apr fixed

--- src/adapters/exchangers/beefy.service.ts
+++ src/adapters/exchangers/beefy.service.ts
@@ -5,6 +5,7 @@ import { ExchangerRequestError } from '../../exceptions/exchanger.request.error'
 import { ExchangerType } from '../../exchanger/models/inner/exchanger.type';
 import { AdaptersService } from '../adapters.service';
 import { ChainType } from '../../exchanger/models/inner/chain.type';
+import puppeteer from "puppeteer";
 
 const POOLS_MAP = {
   "velodrome-v2-ovn-usd+": {
@@ -22,9 +23,12 @@ const POOLS_MAP = {
   }
 }
 
+const TIME_FOR_TRY = 5_000; // 10 sec.
+
 @Injectable()
 export class BeefylService {
   private readonly logger = new Logger(BeefylService.name);
+  BASE_URL = 'https://app.beefy.finance';
   BASE_API_URL = 'https://api.beefy.finance';
   METHOD_GET_PAIRS = 'lps/breakdown';
 
@@ -89,7 +93,7 @@ export class BeefylService {
     return await response;
   }
 
-  async getApr(poolName) {
+  /*async getApr(poolName) {
     // https://api.beefy.finance/apy/breakdown
     const url = `${this.BASE_API_URL}/apy/breakdown`;
     console.log("Load data by url:", url);
@@ -109,5 +113,108 @@ export class BeefylService {
         }
         return String(apr * 100);
       });
+  }*/
+
+  async getApr(poolName):Promise<string> {
+      const url = `${this.BASE_URL}/vault/${poolName}`;
+
+      // Launch a headless browser
+      const browser = await puppeteer.launch(
+        {
+          headless: true,
+          ignoreHTTPSErrors :true,
+          executablePath: '/usr/bin/google-chrome',
+          args: ['--no-sandbox']
+        }
+      );
+
+      this.logger.debug("Browser is start. " + ExchangerType.AERODROME);
+
+      try {
+
+        // Create a new page
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 800 });
+        await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36');
+        // Set a default timeout of 20 seconds
+        await page.setDefaultTimeout(60000);
+
+        // Navigate to the SPA
+        await page.goto(url);
+
+        console.log("GOTO: ", url);
+        // await new Promise(resolve => setTimeout(resolve, TIME_FOR_TRY));
+
+        const textToWaitFor = 'APY breakdown';
+        // await page.waitForSelector(`:contains("${textToWaitFor}")`);
+        console.log("Wait for: ", textToWaitFor);
+        // const textToWaitFor = 'Text you want to wait for';
+
+        await page.waitForFunction(
+          (text) => document.querySelector('body').textContent.includes(text),
+          {},
+          textToWaitFor
+        );
+
+        await new Promise(resolve => setTimeout(resolve, TIME_FOR_TRY));
+
+        console.log("Wait for: ", textToWaitFor, " is finished");
+
+        // Extract the data from the page
+        const data = await page.evaluate(() => {
+          const markerListOfData = '.MuiPaper-root.MuiPaper-elevation1.MuiPaper-rounded';
+          let aprData = null;
+
+          const blocks = document.querySelectorAll(markerListOfData);
+          for (let i = 0; i < blocks.length; i++) {
+            const block = blocks[i];
+            if (!block.textContent.includes('Strategy')) {
+              console.log("Block without apr data: ", block.textContent)
+              continue;
+            }
+
+            aprData = block.textContent;
+            break;
+          }
+
+          console.log("Block with apr data: ", aprData);
+
+          return aprData;
+        });
+
+        // Display the extracted data
+        console.log(data);
+        let str: string = data;
+        this.logger.log("String: from browser", str);
+        if (!str) {
+          this.logger.error("String with apr is empty");
+          return null
+        }
+
+        // Extracting name: The name is at the beginning of the string and ends just before first –.
+        this.logger.log("Start search APY")
+        this.logger.log(str)
+
+        // StrategyStrategy addressVault addressThe vault deposits the user's vAMM-OVN/USD+ in a Aerodrome farm, earning the platform's governance token. Earned token is swapped for more of the underlying assets in order to acquire more of the same liquidity token. To complete the compounding cycle, the new vAMM-OVN/USD+ is added to the farm, ready to go for the next earning event. The transaction cost required to do all this is socialized among the vault's users.APY breakdownTOTAL APY130,847%Vault APR718.92%AuditedCommunity Audit
+        // parse this string and get APY
+        str = str.replace(/,/g, "");
+        const aprRegex = /(?<=TOTAL APY)[0-9.]+(?=%)/;
+        const match = str.match(aprRegex);
+        if (!match || !match.length) {
+          this.logger.error(`APY not found. String: ${str}`);
+          return null;
+        }
+
+        const apr = parseFloat(match[0]);
+        this.logger.log("apr: " + apr)
+        return String(apr);
+      } catch (e) {
+        const errorMessage = `Error when load ${ExchangerType.AERODROME} pairs. url: ${url}`;
+        this.logger.error(errorMessage, e);
+        throw new ExchangerRequestError(errorMessage);
+      } finally {
+        this.logger.debug("Browser is close. " + ExchangerType.AERODROME);
+        await browser.close();
+      }
   }
 }
