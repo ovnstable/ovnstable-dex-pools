a21c854dce5e5d7d4a839c3d70c0c4250892e787
matveyb <matvey.b2022@gmail.com>
2024-01-20 11:59:46 +0100

    pancake aprs added

--- src/adapters/exchangers/pancake.service.ts
+++ src/adapters/exchangers/pancake.service.ts
@@ -5,6 +5,10 @@ import { ExchangerType } from "../../exchanger/models/inner/exchanger.type";
 import { ExchangerRequestError } from "../../exceptions/exchanger.request.error";
 import { ChainType } from "../../exchanger/models/inner/chain.type";
 import BigNumber from "bignumber.js";
+import puppeteer from "puppeteer";
+import { getAgent } from "../../utils/consts";
+
+const TIME_FOR_TRY = 30_000; // 5 sec.
 
 @Injectable()
 export class PancakeService {
@@ -12,6 +16,7 @@ export class PancakeService {
 
     // get all api info / api data
     BASE_GRAPHQL_URL = "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-arb";
+    BASE_URL = "https://pancakeswap.finance/farms?chain=arb";
 
     async getPoolsData(): Promise<PoolData[]> {
         const poolsToLoad = `[\"0xd01075f7314a6436e8b74fc18069848229d0c555\", \"0xb9c2d906f94b27bc403ab76b611d2c4490c2ae3f\"]`
@@ -66,7 +71,7 @@ export class PancakeService {
             })
         })
             .then(async (data): Promise<PoolData[]> => {
-                const pools: PoolData[] = [];
+                let pools: PoolData[] = [];
                 const [responseBody] = await Promise.all([data.json()]);
                 const apiPoolsData = responseBody.data.pools;
 
@@ -78,13 +83,21 @@ export class PancakeService {
                     poolData.tvl = new BigNumber(item.totalValueLockedToken0).plus(item.totalValueLockedToken1).toFixed(2);
 
 
-                    poolData.apr = "1";
+                    poolData.apr = "0";
                     poolData.chain = ChainType.ARBITRUM;
                     pools.push(poolData);
                     this.logger.log(`=========${ExchangerType.PANCAKE}=========`);
                     this.logger.log("Found ovn pool: ", poolData);
                     this.logger.log("==================");
                 });
+
+
+                try {
+                    pools = await this.initAprs(pools);
+                    return pools
+                } catch (e) {
+                    this.logger.error("Error when load apr for " + ExchangerType.PANCAKE);
+                }
 
                 return pools
             })
@@ -96,4 +109,103 @@ export class PancakeService {
 
         return await response;
     }
+
+
+    private async initAprs(ovnPools: PoolData[]): Promise<PoolData[]> {
+        const url = `${this.BASE_URL}`;
+
+        // Launch a headless browser
+        const browser = await puppeteer.launch(
+            {
+                headless: true,
+                ignoreHTTPSErrors: true,
+                executablePath: getAgent(process.env.IS_MAC),
+                args: ["--no-sandbox"]
+            }
+        );
+
+        this.logger.debug("Browser is start. " + ExchangerType.PANCAKE);
+
+        try {
+
+            // Create a new page
+            const page = await browser.newPage();
+            await page.setViewport({ width: 1280, height: 800 });
+            await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            // Set a default timeout of 20 seconds
+            await page.setDefaultTimeout(60000);
+
+            // Navigate to the SPA
+            await page.goto(url);
+            const markerOfLoadingIsFinish = "#table-container";
+
+
+            // Wait for the desired content to load
+            await page.waitForSelector(markerOfLoadingIsFinish);
+            console.log(`Wait ${TIME_FOR_TRY / 1000} seconds`);
+            await new Promise(resolve => setTimeout(resolve, TIME_FOR_TRY));
+
+
+            // Extract the data from the page
+            const data = await page.evaluate(() => {
+                const markerListOfData = "#table-container tr";
+
+                // This function runs in the context of the browser page
+                // You can use DOM manipulation and JavaScript to extract the data
+                const elements = document.querySelectorAll(markerListOfData);
+                const extractedData = [];
+
+                console.log("Elements: ", elements);
+                elements.forEach(element => {
+                    extractedData.push(element.textContent);
+                });
+
+                return extractedData;
+            });
+
+            console.log(`Data from browser: ${ExchangerType.PANCAKE}`, data);
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i];
+                const str: string = element;
+                this.logger.log("String: " + str);
+                if (!str) {
+                    continue;
+                }
+                const regex =/((US[DCTe+.]+))-USD+\D*(\d+\.\d+)\D*(\d+)APR(\d+)/;
+
+                // USDT+ / USD+ percentage values
+                const match = str.match(regex);
+                console.log(match, 'match PANCAKE')
+                const apr = match ? match[5] : null;
+                const poolParsedApr = match ? parseFloat(apr) : null;
+                
+                
+                if (!match || !poolParsedApr) {
+                    continue;
+                }
+
+                // case for 2 pools on pancake
+                const USDC = match[1] === "USDC.e" ? "USDC" : match[1]
+                const pairSymbols = USDC === "USDC" ? `USD+/${USDC}` : `${match[1]}/USD+`;
+
+                ovnPools.forEach(pool => {
+                    if (pool.name === pairSymbols) {
+                        this.logger.log("Find pool for apr update: " + pool.address + " | " + pool.name);
+                        pool.apr = poolParsedApr ? poolParsedApr.toFixed(2) : null;
+                    }
+                });
+            }
+
+            return ovnPools;
+        } catch (e) {
+            const errorMessage = `Error when load ${ExchangerType.PANCAKE} pairs. url: ${url}`;
+            this.logger.error(errorMessage, e);
+            throw new ExchangerRequestError(errorMessage);
+        } finally {
+            this.logger.debug("Browser is close. " + ExchangerType.PANCAKE);
+            await browser.close();
+        }
+
+    }
+
 }
