9f2e7b7bc75a6365d9373bb604bdedb43ce64d6e
Leonid <zotovleonid@icloud.com>
2024-03-22 14:26:11 +0300

    add usd+/crvUSD

--- src/adapters/exchangers/curve.service.ts
+++ src/adapters/exchangers/curve.service.ts
@@ -8,31 +8,33 @@ import {ChainType} from "../../exchanger/models/inner/chain.type";
 import BigNumber from "bignumber.js";
 
 const STABLE_POOLS = [
-    "0x1446999b0b0e4f7ada6ee73f2ae12a2cfdc5d9e7"
+    "0x1446999b0b0e4f7ada6ee73f2ae12a2cfdc5d9e7", //ARB USD+/USDT+
+    "0xda3de145054ed30ee937865d31b500505c4bdfe7" //BASE USD+/crvUSD
 ]
 
 @Injectable()
 export class CurveService {
     private readonly logger = new Logger(CurveService.name);
-    BASE_API_URL = 'https://api.curve.fi/api/';
-    STABLE_POOLS_DATA = 'https://api.curve.fi/api/getPools/arbitrum/factory-stable-ng';
-    STABLE_POOLS_APR = 'https://api.curve.fi/api/getVolumes/arbitrum';
 
-    METHOD = 'getPools';
+    BASE_API_URL = 'https://api.curve.fi/api';
+    GET_POOLS = 'getPools';
+    GET_VOLUMES= 'getVolumes';
     FACTORY = 'factory';
-    BASE_CHAIN = 'base';
+    STABLE_FACTORY = 'factory-stable-ng';
 
     async getPoolsData(): Promise<PoolData[]> {
         const arbitrumPoolsData = await this.loadPoolsData(ChainType.ARBITRUM);
-        const optimismPools = await this.loadPoolsData(ChainType.OPTIMISM);
-        const basePools = await this.loadBasePoolsData();
-        const arbitrumStablePools = await this.loadStablePools();
-        return [...arbitrumPoolsData, ...optimismPools, ...basePools, ...arbitrumStablePools];
+        const arbitrumStablePoolsData = await this.loadStablePoolsData(ChainType.ARBITRUM);
+
+        const optimismPoolsData = await this.loadPoolsData(ChainType.OPTIMISM);
+
+        const basePoolsData = await this.loadPoolsData(ChainType.BASE);
+        const baseStablePoolsData = await this.loadStablePoolsData(ChainType.BASE);
+        return [...arbitrumPoolsData, ...arbitrumStablePoolsData, ...optimismPoolsData, ...basePoolsData, ...baseStablePoolsData];
     }
     
     async loadPoolsData(chainType: ChainType): Promise<PoolData[]> {
-        const url = `${this.BASE_API_URL}/${this.METHOD}/${chainType.toLocaleLowerCase()}/${this.FACTORY}`;
-
+        const url = `${this.BASE_API_URL}/${this.GET_POOLS}/${chainType.toLocaleLowerCase()}/${this.FACTORY}`;
         console.log("Load data by url:", url);
 
         const response = axios
@@ -86,66 +88,19 @@ export class CurveService {
         return await response;
     }
 
-    async loadBasePoolsData(): Promise<PoolData[]> {
-        const url = `${this.BASE_API_URL}/${this.METHOD}/${this.BASE_CHAIN}/${this.FACTORY}`;
-        console.log("Load data by url:", url);
+    async loadStablePoolsData(chainType: ChainType): Promise<PoolData[]> {
+        const url = `${this.BASE_API_URL}/${this.GET_POOLS}/${chainType.toLocaleLowerCase()}/${this.STABLE_FACTORY}`;
+        const apr_url = `${this.BASE_API_URL}/${this.GET_VOLUMES}/${chainType.toLocaleLowerCase()}`;
 
-        const response = axios
-            .get(url, {
-                timeout: 80_000, // 80 sec
-            })
-            .then((data): PoolData[] => {
-                const pools: PoolData[] = [];
-//                 console.log('Response data: ', data.data);
-                const pairs = data.data.data.poolData;
-                let itemCount = 0;
-                pairs.forEach((item) => {
-                    if (
-                        item &&
-                        item.symbol &&
-                        (AdaptersService.OVN_POOLS_NAMES.some((str) =>
-                                item.symbol.toLowerCase().includes(str)) ||
-                            item.symbol.toLowerCase().includes('OVERNIGHT'.toLowerCase()))
-                    ) {
-                        const poolData: PoolData = new PoolData();
-                        let total_apr = new BigNumber(0);
+        console.log("Load pools data by url:", url);
+        console.log("Load stable apr by url:", apr_url);
 
-                        if (item.gaugeRewards && item.gaugeRewards.length > 0){
-                            total_apr = total_apr.plus(item.gaugeRewards[2].apy ?? 0)
-                        }
-
-                        poolData.address = item.address;
-                        poolData.name = item.coins[0].symbol + '/' + item.coins[1].symbol
-                        poolData.tvl = (item.usdTotal).toString();
-                        poolData.apr = total_apr.toFixed(2);
-                        poolData.chain = ChainType.BASE;
-                        pools.push(poolData);
-                        this.logger.log(`========= ${ExchangerType.CURVE} =========`);
-                        itemCount++;
-                        this.logger.log('Found ovn pool #: ', itemCount);
-                        this.logger.log('Found ovn pool: ', poolData);
-                        this.logger.log('==================');
-                    }
-                });
-
-                return pools;
-            })
-            .catch((e) => {
-                const errorMessage = `Error when load ${ExchangerType.CURVE} pairs.`;
-                this.logger.error(errorMessage, e);
-                throw new ExchangerRequestError(errorMessage);
-            });
-
-        return await response;
-    }
-
-    async loadStablePools(): Promise<PoolData[]> {
-        const responsePoolsApr = await axios.get(this.STABLE_POOLS_APR, {
+        const responsePoolsApr = await axios.get(apr_url, {
             timeout: 80_000, // 80 sec
         })
 
         const responsePoolsData = await axios
-            .get(this.STABLE_POOLS_DATA, {
+            .get(url, {
                 timeout: 80_000, // 80 sec
             })
             .then((data): PoolData[] => {
@@ -158,20 +113,21 @@ export class CurveService {
                         const poolData: PoolData = new PoolData();
                         let total_apr = new BigNumber(0);
 
-                        console.log(item)
-
                         if (aprPool?.latestWeeklyApyPcent){
                             total_apr = total_apr.plus(aprPool?.latestWeeklyApyPcent)
                         }
                         if(item.gaugeRewards && item.gaugeRewards.length > 0){
                             total_apr = total_apr.plus(item.gaugeRewards[0].apy ?? 0)
                         }
+                        if (item.gaugeCrvApy && item.gaugeCrvApy.length > 0){
+                            total_apr = total_apr.plus(item.gaugeCrvApy[0] ?? 0)
+                        }
 
                         poolData.address = item.address;
                         poolData.name = item.coins[0].symbol + '/' + item.coins[1].symbol
                         poolData.tvl = (item.usdTotal).toString();
                         poolData.apr = total_apr.toFixed(2);
-                        poolData.chain = ChainType.ARBITRUM;
+                        poolData.chain = chainType;
                         pools.push(poolData);
                         this.logger.log(`========= ${ExchangerType.CURVE} =========`);
                         this.logger.log('Found ovn pool: ', poolData);
